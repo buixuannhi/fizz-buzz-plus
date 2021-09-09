2cde3ab69b559dddfa778ab719d93686b0fde1d6
bui nhi <buixuannhi@gmail.com>
2021-09-09 11:59:57 +0700

    update fizz buzz

--- fizz_buzz_plus.php
+++ fizz_buzz_plus.php
@@ -2,7 +2,7 @@
 for($i =0 ; $i <101 ; $i ++){
     $f=$i%10;
     $b=$i/10;
-    $c=0;
+    $c=$d=0;
     //căn lề
     if($i<10) echo "&emsp;";
     elseif($i <100)echo "&ensp;";
@@ -20,12 +20,13 @@ for($i =0 ; $i <101 ; $i ++){
         case true:
             if($i%5 ==0){
                 echo $i. "buzz";
+                $d++;
             }
         case true:
             if($c >0) break;
+            if($d >0) break;
           elseif($b >3 && $b<4 || $f==3) echo $i."fizz";
+          elseif($b >5 && $b<6 || $f==5) echo $i."buzz";
           break;
    }
 }
-
-?>
